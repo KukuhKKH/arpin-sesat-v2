b452dce5dafade140d43501694a9b9db9a79d658
Kukuh Rahmadani <krahmadani1@gmail.com>
2021-06-11 23:05:20 +0700

    Dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.master')
 
 @section('content')
-Selamat Datang
+<div class="row">
+    <div class="col-md-12">
+        {{-- <div class="panel panel-filled" id="panel-loader"> --}}
+            {{-- <div class="container"> --}}
+                <h2>Selamat Datang</h2>
+                <div class="row">
+                    <div class="col-md-6">
+                        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/Lvz0xk2IWxY?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                    <div class="col-md-6">
+                        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/R4xqPngPsrw?controls=0"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                    </div>
+                    <div class="col-md-6">
+                        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/tSlJDGQKAMA?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                    <div class="col-md-6">
+                        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/18TXgQ5dgoA?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
+            {{-- </div> --}}
+        {{-- </div> --}}
+    </div>
+</div>
 @endsection
